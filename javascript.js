a8ce609dbdcab220eb52a62f19f152d1c491103b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-07-17 12:24:23 -0700

    Create del button/num functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,28 @@
 let num1;
 let operator;
 let num2;
+const output = document.querySelector(".output");
+const calcButtons = document.querySelectorAll(".buttons div button");
+
+calcButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.textContent === "AC") output.textContent = "0";
+        if (output.textContent.length < 11) {
+            if (button.textContent === "DEL" && output.textContent.length > 1) {
+                let sliced = output.textContent.slice(0, -1);
+                output.textContent = sliced;
+            } else if (button.textContent === "DEL" && output.textContent.length === 1) {
+                output.textContent = "0"
+            }
+            if (button.className === "operator") output.textContent += button.textContent;
+            if (button.classList.contains("num") && output.textContent === "0") {
+                output.textContent = button.textContent;
+            } else if (button.classList.contains("num") && output.textContent !== "0") {
+                output.textContent += button.textContent;
+            }  
+        }
+    });
+});
 
 function operate(op, a, b) {
     if (op == "+") return add(a, b);
