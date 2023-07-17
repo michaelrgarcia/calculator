5de300dd8320c541ef011425c58cccb3a8724638
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-07-17 14:28:49 -0700

    Finish basic calculator functioning

--- javascript.js
+++ javascript.js
@@ -1,36 +1,34 @@
-let num1 = null;
+let num1 = 0;
 let operator = null;
-let num2 = null;
-let reset = false;
+let num2 = 0;
 const output = document.querySelector(".output");
 const calcButtons = document.querySelectorAll(".buttons div button");
 
 calcButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.textContent === "AC") {
-            num1 = null;
-            num2 = null;
-            operator = null;
+            clear();
+            num1 = 0;
             output.textContent = "0";
-        } 
+        }
         if (output.textContent.length < 11) {
-            let sliced = output.textContent.slice(0, -1);
-            if (button.textContent === "DEL" && output.textContent.length > 1) {
-                output.textContent = sliced;
-            } else if (button.textContent === "DEL" && output.textContent.length === 1) {
-                output.textContent = "0"
-            }
-            if (button.className === "operator") {
+            if (button.textContent === "DEL") del();
+            if (button.className === "operator" && operator === null) {
                 operator = button.textContent;
-                button.style.background = "black"
-                reset = true;
-            } 
-                if ((button.classList.contains("num") && output.textContent === "0") || reset === true) {
-                    output.textContent = button.textContent;
-                    reset = false;
-                } else if (button.classList.contains("num") && output.textContent.length >= 1)  {
-                    output.textContent += button.textContent;
-                }  
+                console.log(operator)
+            }
+            if ((button.classList.contains("num") && output.textContent === "0") || button.classList.contains("num") && operator !== null && num2 === 0) {
+                output.textContent = button.textContent
+            } else if (button.classList.contains("num") && output.textContent !== "0") {
+                output.textContent += button.textContent;
+            }
+            if (button.className === "equal" && operator !== null) {
+                output.textContent = operate(operator, +num1, +num2);
+                clear();
+            }
+            
+
+    /*         
             if (button.className === "equal" && operator !== null && num1 !== null) {
                 output.textContent = operate(operator, +num1, +num2);  
                 operator = null;
@@ -38,6 +36,9 @@ calcButtons.forEach((button) => {
                 num2 = null;
 
             }
+    */
+        }
+        if (button.classList.contains("num")) {
             if (operator === null) {
                 num1 = output.textContent
                 console.log(num1);
@@ -48,6 +49,20 @@ calcButtons.forEach((button) => {
         }
     });
 });
+
+function clear() {
+    num2 = 0;
+    operator = null;
+}
+
+function del() {
+    let sliced = output.textContent.slice(0, -1);
+    if (output.textContent.length > 1) {
+        output.textContent = sliced;
+    } else if (output.textContent.length === 1) {
+        output.textContent = "0"
+    }
+}
 
 function operate(op, a, b) {
     if (op == "+") return add(a, b);
