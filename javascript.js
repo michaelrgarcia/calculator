970964c9e8576c24cab37b9a1d21445fdac33d19
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-07-19 12:34:46 -0700

    Organize if statements / conditions
    Can't fit under 50 chars, but also fixed the equal button

--- javascript.js
+++ javascript.js
@@ -1,71 +1,84 @@
-const calcButtons = document.querySelectorAll(".buttons div button");
+const numButtons = document.querySelectorAll(".buttons div .num");
+const operatorButtons = document.querySelectorAll(".buttons div .operator");
+const controlButtons = document.querySelectorAll(".buttons div .control");
+const otherButtons = document.querySelectorAll(".buttons div .other");
 const output = document.querySelector(".output");
 let num1 = 0;
 let operator = null;
 let num2 = 0;
-let expressionReset =  false;
+let expressionReset = false;
 
-calcButtons.forEach((button) => {
+controlButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.textContent === "AC") {
             num1 = 0;            
             num2 = 0;
             operator = null;
             output.textContent = "0";
-        }
-        if (output.textContent.length < 11) {
-            if (button.textContent === "DEL") {
-                let sliced = output.textContent.slice(0, -1);
-                if (output.textContent.length > 1) {
-                    output.textContent = sliced;
-                } else if (output.textContent.length === 1) {
-                    output.textContent = "0"
-                }
-            }
-            if (button.classList.contains("operator") && operator === null) {
-                operator = button.textContent;
-                button.classList.remove("unselected");
-                button.classList.add("selected");
-                console.log(operator)
-            } else if (button.classList.contains("operator") && operator !== null && num2 !== 0) {
-                equal();
-                operator = button.textContent;
-                button.classList.remove("unselected");
-                button.classList.add("selected");
-                console.log(operator)
-            }
-            if ((button.classList.contains("num") && 
-                output.textContent === "0") ||
-                (button.classList.contains("num") && operator !== null && num2 === 0) ||
-                (button.classList.contains("num") && expressionReset === true)) {
-                output.textContent = button.textContent;
-            } else if (button.classList.contains("num") && output.textContent !== "0") {
-                output.textContent += button.textContent;
-            }
-            if (button.className === "equal" && operator !== null) equal();
-        }
-        if (button.classList.contains("num")) {
-            if (operator === null) {
-                num1 = output.textContent
-                console.log(num1);
-                expressionReset = false;
-            } else {
-                const operators = document.querySelectorAll(".buttons div .operator")
-                operators.forEach((button) => {
-                    if (button.classList.contains("selected")) {
-                        button.classList.remove("selected");
-                        button.classList.add("unselected");
-                    } 
-                });
-                num2 = output.textContent;
-                console.log(num2);
+        } else if (button.textContent === "DEL" && output.textContent.length < 11) {
+            let sliced = output.textContent.slice(0, -1);
+            if (output.textContent.length > 1) {
+                output.textContent = sliced;
+            } else if (output.textContent.length === 1) {
+                output.textContent = "0"
             }
         }
     });
 });
 
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (output.textContent.length < 11) {
+            if (output.textContent === "0" || (operator !== null && num2 === 0) || expressionReset === true) {
+                output.textContent = button.textContent;
+            } else if (output.textContent !== "0") {
+                output.textContent += button.textContent;
+            }  
+        if (operator === null) {
+            num1 = output.textContent
+            console.log(num1);
+            expressionReset = false;
+        } else {
+            const operators = document.querySelectorAll(".buttons div .operator")
+            operators.forEach((button) => {
+            if (button.classList.contains("selected")) {
+                button.classList.remove("selected");
+                button.classList.add("unselected");
+            } 
+            });
+            num2 = output.textContent;
+            console.log(num2);
+            expressionReset = false;
+        }
+        }
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (operator === null) {
+            operator = button.textContent;
+            button.classList.remove("unselected");
+            button.classList.add("selected");
+            console.log(operator)
+        } else if (operator !== null && num2 !== 0) {
+            equal();
+            operator = button.textContent;
+            button.classList.remove("unselected");
+            button.classList.add("selected");
+            console.log(operator)
+        }
+    });
+});
+
+otherButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.classList.contains("equal") && operator !== null) equal();
+    });
+});
+
 function equal() {
-    output.textContent = +operate(operator, +num1, +num2).toFixed(8);
+    output.textContent = +operate(operator, +num1, +num2).toFixed(6);
     num1 = output.textContent;
     num2 = 0;
     operator = null;
@@ -73,7 +86,6 @@ function equal() {
 }
 
 function operate(op, a, b) {
-    expressionReset = true;
     if (op === "/" && a === 0 && b === 0) return "no";
     if (op == "+") return add(a, b);
     if (op == "-") return subtract(a, b);
